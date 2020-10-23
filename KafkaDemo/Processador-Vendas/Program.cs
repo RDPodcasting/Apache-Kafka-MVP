25316682b4cd09c06b0bf377ce46c37b6c9b07bb
Thiago de Melo Lima <56129760+RDPodcasting@users.noreply.github.com>
2020-10-23 02:58:19 -0300

    Kafka Demo
    commit inicial

---
+++ KafkaDemo/Processador-Vendas/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using Serilog;
+
+namespace Processador_Vendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Information("Testando o consumo de mensagens com Kafka");
+
+            string bootstrapServer = "localhost:9092";
+            string topic = "teste";
+
+            logger.Information($"Servidor = {bootstrapServer}");
+            logger.Information($"Tópico = {topic}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServer,
+                GroupId = $"{topic}-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(topic);
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            logger.Information(
+                                $"Mensagem lida: {cr.Message.Value}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                        logger.Warning("Cancelada a execução do Consumer...");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ KafkaDemo/Vendas-Ingressos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace Vendas_Ingressos
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Carregando configurações...");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+            var configuration = builder.Build();
+
+            var logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .CreateLogger();
+            Console.WriteLine("Configurações carregadas com sucesso...");
+
+
+            logger.Information("Testando o envio de mensagens com Kafka");
+
+            string bootstrapServer = configuration["Kafka_Broker"]; ;
+            string topic = configuration["Kafka_Topic"];
+
+            logger.Information($"BootstrapServer = {bootstrapServer}");
+            logger.Information($"Topic = {topic}");
+
+            try
+            {
+                ProducerConfig config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServer
+                };
+
+                using var producer = new ProducerBuilder<Null, string>(config).Build();
+                
+                    for (int i = 0; i < int.Parse(configuration["TotalVendas"]); i++)
+                    {
+                        var result = await producer.ProduceAsync(
+                            topic,
+                            new Message<Null, string>
+                            { Value = $"venda {i}" });
+
+                        logger.Information(
+                            $"Mensagem: {$"venda {i}"} | " +
+                            $"Status: { result.Status}");
+                    }
+                logger.Information("Concluído o envio de mensagens");
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+        }
+
+
+    }
+}
